82e95317ccff50ee4d80bffcf11e582a05874fa3
oriky24 <0riturgenan2@gmail.com>
2024-12-09 15:45:17 +0200

    נסיון 1

--- codons.py
+++ codons.py
@@ -1,4 +1,9 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
-
+  amino_acids_dict = {}
+  for row in rows:
+      codon_row = row.strip().split('\t')
+      codon = codon_row[0]
+      amino_acid = codon_row[2]
+      amino_acids_dict[codon] = amino_acid
+  return amino_acids_dict
 
