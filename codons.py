96102a92abc095497a0d58aa31ff07306550e5e9
oriky24 <0riturgenan2@gmail.com>
2024-12-09 15:52:57 +0200

    נסיון 2

--- codons.py
+++ codons.py
@@ -1,9 +1,12 @@
 def create_codon_dict(file_path):
-  amino_acids_dict = {}
-  for row in rows:
-      codon_row = row.strip().split('\t')
-      codon = codon_row[0]
-      amino_acid = codon_row[2]
-      amino_acids_dict[codon] = amino_acid
-  return amino_acids_dict
-
+    path = "data/codons.txt"
+    with open(path, 'r') as file:
+         rows = file.readlines()
+    
+    amino_acids_dict = {}
+    for row in rows:
+        codon_row = row.strip().split('\t')
+        codon = codon_row[0]
+        amino_acid = codon_row[2]
+        amino_acids_dict[codon] = amino_acid
+    return amino_acids_dict
